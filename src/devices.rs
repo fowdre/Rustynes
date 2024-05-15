58a1af569e196b94300968ac6418855c79ac4415
Fowdre <foudre1000@gmail.com>
2024-05-15 11:00:05 +0200

    Moved Flags enum locaton

--- src/devices.rs
+++ src/devices.rs
@@ -5,17 +5,6 @@ pub use cpu6502::Cpu6502;
 
 pub mod cpu6502 {
     use crate::bus;
-    
-    enum Flags {
-        C = (1 << 0), // Carry
-        Z = (1 << 1), // Zero
-        I = (1 << 2), // Interrupt Disable
-        D = (1 << 3), // Decimal
-        B = (1 << 4),
-        U = (1 << 5),
-        V = (1 << 6), // Overflow
-        N = (1 << 7), // Negative
-    }
 
     #[derive(Debug, Default)]
     pub struct Cpu6502<'a> {
@@ -34,6 +23,17 @@ pub mod cpu6502 {
         cycles: u8,
     }
 
+    enum Flags {
+        C = (1 << 0), // Carry
+        Z = (1 << 1), // Zero
+        I = (1 << 2), // Interrupt Disable
+        D = (1 << 3), // Decimal
+        B = (1 << 4),
+        U = (1 << 5),
+        V = (1 << 6), // Overflow
+        N = (1 << 7), // Negative
+    }
+    
     impl<'a> Cpu6502<'a> {
         fn connect_bus(&mut self, bus: bus::Bus<'a>) {
             self.bus = Some(bus);
