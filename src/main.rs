8e0f2da3f648c92741716db2ab11cbe973801763
Fowdre <foudre1000@gmail.com>
2024-05-17 00:30:30 +0200

    Moved main body into tests.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
+mod tests;
+
 mod nes;
 use nes::NES;
 
 fn main() {
-    let mut nes = NES::new();
+    let nes = NES::new();
 
     println!("{:?}", nes);
-
-    nes.cpu_write(0x0000, 0x0001);
-    nes.cpu_write(0x0001, 0x0002);
-    nes.cpu_write(0x0004, 0x0003);
-
-    for i in 0..5 {
-        let data = nes.cpu_read(i);
-        println!("Data byte [0x{:04X}] -> [0x{:02X}]", i, data);
-    }
 }
---
+++ src/tests.rs
@@ -0,0 +1,24 @@
+#[cfg(test)]
+
+use super::*;
+
+#[test]
+fn test_cpu_read_and_write() {
+    let mut nes = NES::new();
+
+    assert_eq!(nes.cpu_read(0x0000), 0x00);
+    assert_eq!(nes.cpu_read(0x0001), 0x00);
+    assert_eq!(nes.cpu_read(0x0002), 0x00);
+    assert_eq!(nes.cpu_read(0x0003), 0x00);
+    assert_eq!(nes.cpu_read(0x0004), 0x00);
+
+    nes.cpu_write(0x0000, 0x0001);
+    nes.cpu_write(0x0001, 0x0002);
+    nes.cpu_write(0x0004, 0x0003);
+
+    assert_eq!(nes.cpu_read(0x0000), 0x01);
+    assert_eq!(nes.cpu_read(0x0001), 0x02);
+    assert_eq!(nes.cpu_read(0x0002), 0x00);
+    assert_eq!(nes.cpu_read(0x0003), 0x00);
+    assert_eq!(nes.cpu_read(0x0004), 0x03);
+}
