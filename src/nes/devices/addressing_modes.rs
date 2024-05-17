88f497e49f3088b280cffd563cb9716e48c4e170
Fowdre <foudre1000@gmail.com>
2024-05-17 04:22:23 +0200

    Removed '_' where it wasn't needed

--- src/nes/devices/addressing_modes.rs
+++ src/nes/devices/addressing_modes.rs
@@ -18,11 +18,11 @@ impl Cpu6502 {
     }
     
     /// Absolute addressing mode
-    pub fn addr_ABS(&mut self, _bus: &Bus) -> u8 {
-        let lo = self.read(_bus, self.pc as u16) as u16;
+    pub fn addr_ABS(&mut self, bus: &Bus) -> u8 {
+        let lo = self.read(bus, self.pc as u16) as u16;
         self.pc += 1;
         
-        let hi = self.read(_bus, self.pc as u16) as u16;
+        let hi = self.read(bus, self.pc as u16) as u16;
         self.pc += 1;
         
         self.addr_abs = (hi << 8) | lo;
@@ -31,11 +31,11 @@ impl Cpu6502 {
     }
     
     /// Absolute addressing mode with X offset
-    pub fn addr_ABSx(&mut self, _bus: &Bus) -> u8 {
-        let lo = self.read(_bus, self.pc as u16) as u16;
+    pub fn addr_ABSx(&mut self, bus: &Bus) -> u8 {
+        let lo = self.read(bus, self.pc as u16) as u16;
         self.pc += 1;
         
-        let hi = self.read(_bus, self.pc as u16) as u16;
+        let hi = self.read(bus, self.pc as u16) as u16;
         self.pc += 1;
         
         self.addr_abs = (hi << 8) | lo;
@@ -49,11 +49,11 @@ impl Cpu6502 {
     }
     
     /// Absolute addressing mode with Y offset
-    pub fn addr_ABSy(&mut self, _bus: &Bus) -> u8 {
-        let lo = self.read(_bus, self.pc as u16) as u16;
+    pub fn addr_ABSy(&mut self, bus: &Bus) -> u8 {
+        let lo = self.read(bus, self.pc as u16) as u16;
         self.pc += 1;
         
-        let hi = self.read(_bus, self.pc as u16) as u16;
+        let hi = self.read(bus, self.pc as u16) as u16;
         self.pc += 1;
         
         self.addr_abs = (hi << 8) | lo;
@@ -76,8 +76,8 @@ impl Cpu6502 {
     }
     
     /// Zero Page addressing mode with X offset
-    pub fn addr_ZPGx(&mut self, _bus: &Bus) -> u8 {
-        self.addr_abs = self.read(_bus, self.pc as u16) as u16 + self.x as u16;
+    pub fn addr_ZPGx(&mut self, bus: &Bus) -> u8 {
+        self.addr_abs = self.read(bus, self.pc as u16) as u16 + self.x as u16;
         self.pc += 1;
         self.addr_abs &= 0x00FF;
         
@@ -85,8 +85,8 @@ impl Cpu6502 {
     }
     
     /// Zero Page addressing mode with Y offset
-    pub fn addr_ZPGy(&mut self, _bus: &Bus) -> u8 {
-        self.addr_abs = self.read(_bus, self.pc as u16) as u16 + self.y as u16;
+    pub fn addr_ZPGy(&mut self, bus: &Bus) -> u8 {
+        self.addr_abs = self.read(bus, self.pc as u16) as u16 + self.y as u16;
         self.pc += 1;
         self.addr_abs &= 0x00FF;
         
@@ -99,8 +99,8 @@ impl Cpu6502 {
     }
     
     /// Relative addressing mode
-    pub fn addr_REL(&mut self, _bus: &Bus) -> u8 {
-        self.addr_rel = self.read(_bus, self.pc as u16) as u16;
+    pub fn addr_REL(&mut self, bus: &Bus) -> u8 {
+        self.addr_rel = self.read(bus, self.pc as u16) as u16;
         self.pc += 1;
         
         if (self.addr_rel & 0x80) != 0 {
@@ -112,31 +112,31 @@ impl Cpu6502 {
     
     /// Indirect addressing mode
     /// (implements a hardware bug)
-    pub fn addr_IND(&mut self, _bus: &Bus) -> u8 {
-        let ptr_lo = self.read(_bus, self.pc as u16) as u16;
+    pub fn addr_IND(&mut self, bus: &Bus) -> u8 {
+        let ptr_lo = self.read(bus, self.pc as u16) as u16;
         self.pc += 1;
         
-        let ptr_hi = self.read(_bus, self.pc as u16) as u16;
+        let ptr_hi = self.read(bus, self.pc as u16) as u16;
         self.pc += 1;
         
         let ptr = (ptr_hi << 8) | ptr_lo;
         
         if ptr_lo == 0x00FF {
-            self.addr_abs = (self.read(_bus, ptr & 0xFF00) as u16) << 8 | self.read(_bus, ptr) as u16;
+            self.addr_abs = (self.read(bus, ptr & 0xFF00) as u16) << 8 | self.read(bus, ptr) as u16;
         } else {
-            self.addr_abs = (self.read(_bus, ptr + 1) as u16) << 8 | self.read(_bus, ptr) as u16;
+            self.addr_abs = (self.read(bus, ptr + 1) as u16) << 8 | self.read(bus, ptr) as u16;
         }
         
         0
     }
     
     /// Indirect addressing mode with X offset (zero page)
-    pub fn addr_INDx(&mut self, _bus: &Bus) -> u8 {
-        let t = self.read(_bus, self.pc as u16) as u16;
+    pub fn addr_INDx(&mut self, bus: &Bus) -> u8 {
+        let t = self.read(bus, self.pc as u16) as u16;
         self.pc += 1;
         
-        let lo = self.read(_bus, (t + self.x as u16) & 0x00FF) as u16;
-        let hi = self.read(_bus, (t + self.x as u16 + 1) & 0x00FF) as u16;
+        let lo = self.read(bus, (t + self.x as u16) & 0x00FF) as u16;
+        let hi = self.read(bus, (t + self.x as u16 + 1) & 0x00FF) as u16;
         
         self.addr_abs = (hi << 8) | lo;
         
@@ -144,12 +144,12 @@ impl Cpu6502 {
     }
     
     /// Indirect addressing mode with Y offset (zero page)
-    pub fn addr_INDy(&mut self, _bus: &Bus) -> u8 {
-        let t = self.read(_bus, self.pc as u16) as u16;
+    pub fn addr_INDy(&mut self, bus: &Bus) -> u8 {
+        let t = self.read(bus, self.pc as u16) as u16;
         self.pc += 1;
         
-        let lo = self.read(_bus, t & 0x00FF) as u16;
-        let hi = self.read(_bus, (t + 1) & 0x00FF) as u16;
+        let lo = self.read(bus, t & 0x00FF) as u16;
+        let hi = self.read(bus, (t + 1) & 0x00FF) as u16;
         
         self.addr_abs = (hi << 8) | lo;
         self.addr_abs += self.y as u16;
